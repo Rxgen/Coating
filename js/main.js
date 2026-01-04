4e060e3f0414d6a5238ade880d6b888f17295dd8
Ravi Chauhan <ravi.chauhan@webmaffia.com>
2026-01-04 15:50:54 +0530

    Slider issue is fixed

--- js/main.js
+++ js/main.js
@@ -21,5 +21,83 @@ $(document).ready(function(){
     
 	jQuery('.thumb').nailthumb({width:100,height:100});
 	
+	// Function to check if mobile view
+	function isMobileView() {
+		return $(window).width() < 768;
+	}
+	
+	// Mobile navbar toggle fix
+	$('.navbar-toggle').on('click', function(e) {
+		e.preventDefault();
+		var $target = $($(this).data('target'));
+		var isCollapsed = $target.hasClass('in');
+		
+		if (isCollapsed) {
+			$target.removeClass('in');
+			$(this).addClass('collapsed').attr('aria-expanded', 'false');
+		} else {
+			$target.addClass('in');
+			$(this).removeClass('collapsed').attr('aria-expanded', 'true');
+		}
+	});
+	
+	// Mobile dropdown menu handling - prevent default Bootstrap behavior on mobile
+	$('.navbar-nav .dropdown > a.dropdown-toggle').on('click', function(e) {
+		if (isMobileView()) {
+			e.preventDefault();
+			e.stopPropagation();
+			var $dropdown = $(this).next('.dropdown-menu');
+			var $parent = $(this).parent('.dropdown');
+			var isOpen = $parent.hasClass('open');
+			
+			// Close other open dropdowns
+			$('.navbar-nav .dropdown').not($parent).removeClass('open').find('.dropdown-menu').slideUp(200);
+			
+			// Toggle current dropdown
+			if (isOpen) {
+				$parent.removeClass('open');
+				$dropdown.slideUp(200);
+				$(this).attr('aria-expanded', 'false');
+			} else {
+				$parent.addClass('open');
+				$dropdown.slideDown(200);
+				$(this).attr('aria-expanded', 'true');
+			}
+		}
+	});
+	
+	// Disable hover dropdown on mobile
+	$(window).on('resize', function() {
+		if (isMobileView()) {
+			$('.navbar-nav .dropdown').off('mouseenter mouseleave');
+		}
+	}).trigger('resize');
+	
+	// Close mobile menu when clicking outside
+	$(document).on('click', function(e) {
+		if (isMobileView() && !$(e.target).closest('.navbar').length) {
+			$('#navbar-collapse-menu').removeClass('in');
+			$('.navbar-toggle').addClass('collapsed').attr('aria-expanded', 'false');
+			$('.navbar-nav .dropdown').removeClass('open').find('.dropdown-menu').slideUp(200);
+		}
+	});
+	
+	// Close mobile menu when clicking on a regular link (not dropdown toggle)
+	$('.navbar-nav > li > a:not(.dropdown-toggle)').on('click', function() {
+		if (isMobileView()) {
+			$('#navbar-collapse-menu').removeClass('in');
+			$('.navbar-toggle').addClass('collapsed').attr('aria-expanded', 'false');
+			$('.navbar-nav .dropdown').removeClass('open').find('.dropdown-menu').slideUp(200);
+		}
+	});
+	
+	// Close mobile menu when clicking on dropdown menu items
+	$('.dropdown-menu > li > a').on('click', function() {
+		if (isMobileView()) {
+			$('#navbar-collapse-menu').removeClass('in');
+			$('.navbar-toggle').addClass('collapsed').attr('aria-expanded', 'false');
+			$('.navbar-nav .dropdown').removeClass('open').find('.dropdown-menu').slideUp(200);
+		}
+	});
 	
 });
